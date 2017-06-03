38594f98c79aef61ea6124a7ac84e59c000c3c57
aniket965 <aniket965.as@gmail.com>
2017-06-03 22:58:19 +0530

    added theme,toolbar

--- app/src/main/java/com/scibots/aniket/mtickets/Home_view.kt
+++ app/src/main/java/com/scibots/aniket/mtickets/Home_view.kt
@@ -1,25 +1,33 @@
 package com.scibots.aniket.mtickets
 
+import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.arlib.floatingsearchview.FloatingSearchView
+import android.view.WindowManager
+import android.widget.TextView
+
 
 class Home_view : AppCompatActivity() {
-    private var mSearchView: FloatingSearchView? = null
+
     private var TAG = "HOME_VIEW"
+    private var titleTextview: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
 
-        // Search view
-        mSearchView = findViewById(R.id.floating_search_view) as FloatingSearchView
-        mSearchView?.setOnQueryChangeListener(FloatingSearchView.OnQueryChangeListener { oldQuery, newQuery ->
-            //get suggestions based on newQuery
-            Log.d(TAG, newQuery);
-            //pass them on to the search view
+        // for making status bar completly transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
 
-        });
+        titleTextview = findViewById(R.id.genreTitle) as TextView
+
+        //setting font for the title text
+        val custom_font = Typeface.createFromAsset(assets, "font/VastShadow-Regular.ttf")
+        titleTextview?.typeface = custom_font
+
 
     }
 }

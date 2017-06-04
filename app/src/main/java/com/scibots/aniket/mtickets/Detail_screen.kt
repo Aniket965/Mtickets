8cb2b0eac504bfceb207d420e552c917e3d3f125
aniket965 <aniket965.as@gmail.com>
2017-06-04 21:02:04 +0530

    3rd phase started

---
+++ app/src/main/java/com/scibots/aniket/mtickets/Detail_screen.kt
@@ -0,0 +1,12 @@
+package com.scibots.aniket.mtickets
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Detail_screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_screen)
+    }
+}
--- app/src/main/java/com/scibots/aniket/mtickets/adapters/GenreboxAdapter.kt
+++ app/src/main/java/com/scibots/aniket/mtickets/adapters/GenreboxAdapter.kt
@@ -1,13 +1,17 @@
 package com.scibots.aniket.mtickets.adapters
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Typeface
+import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.scibots.aniket.mtickets.Home_view
 import com.scibots.aniket.mtickets.R
 import com.scibots.aniket.mtickets.dataclass.Movie
 import com.squareup.picasso.Picasso
@@ -31,10 +35,26 @@ class GenreboxAdapter(internal var context: Context, Moviesdataset: ArrayList<Mo
         if (Moviesdataset!!.size > 0) {
             var movie: Movie = Moviesdataset?.get(i)!!
             url = movie.backdrop_path
-
             viewHolder.mTitle.setText(movie.original_title)
             viewHolder.mdisp.setText(movie.overview)
             viewHolder.setImage(url);
+            viewHolder.mButText.setOnClickListener {
+                object : View.OnClickListener {
+                    override fun onClick(view: View): Unit {
+                        var intent: Intent = Intent(context, Home_view::class.java)
+                        var b = Bundle()
+                        b.putInt("movieId", movie.id)
+                        b.putString("movieName", movie.original_title)
+                        b.putString("moviebackurl", movie.backdrop_path)
+                        b.putString("movieOverview", movie.overview)
+                        b.putString("moviedate", movie.release_date)
+                        b.putString("movievotes", movie.vote_average)
+                        intent.putExtras(b)
+                        context.startActivity(intent)
+                        (context as Activity).finish()
+                    }
+                }
+            }
         }
 
     }

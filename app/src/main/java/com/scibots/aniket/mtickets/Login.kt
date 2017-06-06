03cd35f6393736283f13a700ee3855ca1f038ee2
aniket sharma <aniket965.as@gmail.com>
2017-06-07 00:58:39 +0530

    facebook auth

---
+++ app/src/main/java/com/scibots/aniket/mtickets/Login.kt
@@ -0,0 +1,12 @@
+package com.scibots.aniket.mtickets
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
